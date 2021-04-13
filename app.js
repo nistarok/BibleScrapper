e43e2fb9d56a40a87cf734740f470fa4a2f2b8c5
Udo Schmidt Junior <udo.schmidt.jr@gmail.com>
2021-04-13 11:49:38 -0300

    Test commit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const sqlite3 = require('sqlite3');
 
 const sqlite = require('sqlite');
 
-
 const url = "https://www.bibliatodo.com/pt/a-biblia/almeida-revista-atualizada-1993/salmos-138"
 
 async function getChapters(name) {
